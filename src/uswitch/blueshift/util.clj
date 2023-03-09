17f4821c7758463cade3e94fddac9da1ea407a1d
Dave NeSmith <dnesmith@tradeswell.com>
2023-03-09 16:06:55 -0500

    [DATA-411] better logging

--- src/uswitch/blueshift/util.clj
+++ src/uswitch/blueshift/util.clj
@@ -1,6 +1,11 @@
 (ns uswitch.blueshift.util
-  (:require [clojure.core.async :refer (close!)]
-            [clostache.parser :as clo]))
+  (:require [cheshire.core :as json]
+            [clojure.core.async :refer (close!)]
+            [clostache.parser :as clo]
+            [clojure.tools.logging :refer (error)])
+  (:import
+   (java.time Instant)
+   (java.time.format DateTimeFormatter)))
 
 (defn clear-keys
   "dissoc for components records. assoc's nil for the specified keys"
@@ -18,3 +23,20 @@
   [input-str]
   (let [env-map (into {} (map (fn [[k v]] {(keyword k) v}) (System/getenv)))]
     (clo/render input-str env-map)))
+
+(defn create-error-log-msg
+  [err-reason custom-fields]
+  {:log
+   {:level "ERROR"
+    :timestamp (.format DateTimeFormatter/ISO_INSTANT (Instant/now))
+    :module "blueshift"
+    :source (str *ns*)
+    :category "error"
+    :message err-reason}
+   :custom-fields (or custom-fields {})})
+
+(defn log-error
+  ([msg]
+   (log-error msg nil))
+  ([msg data]
+   (error msg (json/generate-string (create-error-log-msg msg data)))))
